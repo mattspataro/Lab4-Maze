4e0a8453adbf4bf3731823bda7ea16691f3e52d0
Matt Spataro <matt_spataro@outlook.com>
2019-10-20 04:34:04 +0000

    DONE

--- Pathfinder.cpp
+++ Pathfinder.cpp
@@ -3,11 +3,13 @@
 #include <vector>
 #include <sstream>
 #include <fstream>
+#include <time.h>
 #include "Pathfinder.h"
 using namespace std;
 
 Pathfinder::Pathfinder() {
-    
+    /* initialize random seed: */
+    srand (time(NULL));
     //Set the default maze to a matrix of all 1's
     for(int z = 0; z < SIZE; z++){
         for(int y = 0; y < SIZE; y++){
@@ -37,7 +39,6 @@ void Pathfinder::copyArray(int a1[5][5][5], int a2[5][5][5]) {
     return;
 }
 string Pathfinder::toString() const{
-
     ostringstream output;
     for(int z = 0; z < SIZE; z++){
         for(int y = 0; y < SIZE; y++){
@@ -56,6 +57,15 @@ string Pathfinder::toString() const{
     return output.str();
 }
 void Pathfinder::createRandomMaze(){
+    for(int z = 0; z < SIZE; z++){
+        for(int y = 0; y < SIZE; y++){
+            for(int x = 0; x < SIZE; x++){
+                maze[x][y][z] = rand() % 2;
+            }
+        }
+    }
+    maze[0][0][0] = 1;
+    maze[4][4][4] = 1;
     return;
 }
 bool Pathfinder::importMaze(string file_name){
@@ -100,9 +110,39 @@ bool Pathfinder::importMaze(string file_name){
     return true;
 }
 bool Pathfinder::findPath(int x, int y, int z){
-    return false;
+    //base cases
+    string coordinate = "("+ to_string(x)+", "+to_string(y)+", "+to_string(z)+")";
+    path.push_back(coordinate);
+    if(x < 0 || x >= SIZE || y < 0 || y >= SIZE || z < 0 || z >= SIZE){
+        //YOU'RE OUT OF BOUNDS!
+        path.pop_back();
+        return false;
+    }
+    if(maze[x][y][z] != 1){
+        //1) YOU'RE ON A WALL OR
+        //2) YOU'VE BEEN HERE BEFORE
+        path.pop_back();
+        return false;
+    }
+    if(x==4 && y==4 && z==4){
+        //YOU MADE IT!!!
+        return true;
+    }
+    maze[x][y][z] = 2;
+    if(findPath(x+1,y,z)||findPath(x-1,y,z)||findPath(x,y+1,z)||findPath(x,y-1,z)||findPath(x,y,z+1)||findPath(x,y,z-1)){
+        return true;
+    }else{
+        path.pop_back();
+        return false;
+    }
 }
 vector<string> Pathfinder::solveMaze(){
-    vector<string> test;
-    return test;
+    //intialize variables
+    path.clear();
+    int temp[5][5][5];
+    //solve the maze
+    copyArray(maze, temp); //save the original maze state temporarily
+    findPath(0,0,0); //call recursive function
+    copyArray(temp, maze); //restores the maze to its original state
+    return path;
 }
--- Pathfinder.h
+++ Pathfinder.h
@@ -15,8 +15,8 @@ public:
 	vector<string> solveMaze();
 private:
     const int SIZE = 5;
-    int maze[5][5][5];
     vector<string> path;
+    int maze[5][5][5];
     bool isValidMaze(int token, int num);
     bool findPath(int x, int y, int z);
     void copyArray(int array1[5][5][5], int array2[5][5][5]);
